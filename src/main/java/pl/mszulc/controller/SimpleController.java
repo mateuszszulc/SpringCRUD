8152fa17c2c943767f2d0086ac7faa46c88c125c
mateuszszulc <mateusz.szulc@gmail.com>
2013-04-29 09:19:34 +0200

    Integrated Hibernate-Generic-DAO. But next step is to migrate to Spring Data JPA.
    Works with MySQL, but not with HSql.

--- src/main/java/pl/mszulc/controller/SimpleController.java
+++ src/main/java/pl/mszulc/controller/SimpleController.java
@@ -1,5 +1,7 @@
 package pl.mszulc.controller;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,7 @@ import pl.mszulc.Shop;
 @Controller
 @RequestMapping("/wine")
 public class SimpleController {
+
 
     @RequestMapping("/home")
     @ResponseBody
